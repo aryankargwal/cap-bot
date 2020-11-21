952befa817c9ef91ad0bb0032248c33643319a6d
aryankargwal <kargwalaryan@gmail.com>
2020-11-21 18:17:59 +0530

    final gui

--- gui/js/init.js
+++ gui/js/init.js
@@ -1,5 +1,8 @@
 (function($){
     $(function(){
   
+        $('select').formSelect();
+        $('.dropdown-trigger').dropdown()
+  
     }); // end of document ready
   })(jQuery); // end of jQuery name space
