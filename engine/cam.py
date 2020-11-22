aaca6f1be48bd25bf539dabe7ee49689b11e69d0
aryankargwal <kargwalaryan@gmail.com>
2020-11-22 10:31:06 +0530

    minor updates

--- engine/cam.py
+++ engine/cam.py
@@ -1,14 +1,11 @@
 import os
 import time 
 import cv2 
-  
-  
-# define a video capture object 
 vid = cv2.VideoCapture(0) 
 i=0  
 
 while(True): 
-      
+    
     # Capture the video frame 
     # by frame 
     ret, frame = vid.read() 
@@ -18,13 +15,13 @@ while(True):
     cv2.imwrite(path+str(i)+'.jpg',frame)
     i+=1
     time.sleep(5)
-      
+    
     # the 'q' button is set as the 
     # quitting button you may use any 
     # desired button of your choice 
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
-  
+
 # After the loop release the cap object 
 vid.release() 
 # Destroy all the windows 
