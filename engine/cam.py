29d1359caf870f7847e1fd4605a1d4dbe38ad852
aryankargwal <kargwalaryan@gmail.com>
2020-11-22 08:55:32 +0530

    cam.py added

--- engine/cam.py
+++ engine/cam.py
@@ -1,18 +1,23 @@
-# import the opencv library 
+import os
+import time 
 import cv2 
   
   
 # define a video capture object 
 vid = cv2.VideoCapture(0) 
-  
+i=0  
+
 while(True): 
       
     # Capture the video frame 
     # by frame 
     ret, frame = vid.read() 
-  
+    path="./images/"
     # Display the resulting frame 
     cv2.imshow('frame', frame) 
+    cv2.imwrite(path+str(i)+'.jpg',frame)
+    i+=1
+    time.sleep(5)
       
     # the 'q' button is set as the 
     # quitting button you may use any 
