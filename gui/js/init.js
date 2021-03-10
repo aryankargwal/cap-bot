8d1ab85fe81ae65d257fcd0f9e0ba69850281b1a
aryankargwal <kargwalaryan@gmail.com>
2021-03-10 21:45:42 +0530

    new branch

--- gui/js/init.js
+++
@@ -1,8 +0,0 @@
-(function($){
-    $(function(){
-  
-        $('select').formSelect();
-        $('.dropdown-trigger').dropdown()
-  
-    }); // end of document ready
-  })(jQuery); // end of jQuery name space
